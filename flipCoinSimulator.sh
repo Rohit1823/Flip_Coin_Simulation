0e79aac40b93daa161082e5f5b9cc08075a3d51e
Rohit1823 <rohitmachale1823@gmail.com>
2021-04-16 10:42:33 +0530

    ADDED : Winning Combination

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,6 +4,18 @@
 
 #Name : Rohit Machale
 
+declare -A singlet
+singlet=( ["H"]=0 ["T"]=0 )
+
+declare -A doublet
+doublet=( ["HH"]=0 ["TT"]=0 ["HT"]=0 ["TH"]=0 )
+
+declare -A triplet
+triplet=( ["HHH"]=0 ["TTT"]=0 ["HHT"]=0 ["TTH"]=0 ["HTH"]=0 ["THT"]=0 ["THH"]=0 ["HTT"]=0 )
+
+timesFlip=0
+choice=0
+
 function flipCoin() {
 	face=$(( RANDOM % 2 ))
 	if [ $face -eq 1 ]
@@ -18,14 +30,25 @@ function flipCoin() {
 
 function percentage() {
 	temp=$1
+	noFlip=$2
 	percent=$(( temp * 100 / 10 ))
 	echo "$percent%"
 }
 
+function maxOneFlip() {
+	for combination in "${!singlet[@]}"
+	do
+		if [[ $maxCount -lt ${singlet[$combination]} ]]
+		then
+			maxCount=${singlet[$combination]}
+			maxCombination=$combination
+		fi
+	done
+	echo "Winning Combination is $maxCombination with $maxCount"
+}
+
 function oneFlip() {
-	declare -A singlet
-	singlet=( ["H"]=0 ["T"]=0 )
-	
+	noFlip=$1
 	singletHeadCount=0
 	singletTailCount=0
 	for (( i=0; i<10; i++ ))
@@ -47,10 +70,20 @@ echo The Percentage of Tail in singlet combination is $( percentage $singletTail
 
 #Doublet
 
-function twoFlip() {
-	declare -A doublet
-	doublet=( ["HH"]=0 ["TT"]=0 ["HT"]=0 ["TH"]=0 )
+function maxTwoFlip() {
+	for combination in "${!doublet[@]}"
+	do
+		if [[ $maxCount -lt ${doublet[$combination]} ]]
+		then
+			maxCount=${doublet[$combination]}
+			maxCombination=$combination
+		fi
+	done
 
+	echo "Winning combination is $maxCombination with $maxCount"
+}
+function twoFlip() {
+	noFlip=$1
 	doubleHeadCount=0
 	doubleTailCount=0
 	headThenTailCount=0
@@ -82,9 +115,19 @@ echo HH percent: $( percentage $doubleHeadCount ), TT percent: $( percentage $do
 
 #Triplet
 
+function maxThreeFlip() {
+	for combination in "${!triplet[@]}"
+	do
+		if [[ $maxCount -lt ${triplet[$combination]} ]]
+		then
+			maxCount=${triplet[$combination]}
+			maxCombination=$combination
+		fi
+	done
+
+	echo "Winning combination is $maxCombination with $maxCount"
+}
 function threeFlip() {
-	declare -A triplet
-	triplet=( [HHH]=0 [TTT]=0 [HHT]=0 [TTH]=0 [HTH]=0 [THT]=0 [THH]=0 [HTT]=0 )
 	noFlip=$1
 	tripleHeadCount=0
 	tripleTailCount=0
@@ -136,19 +179,21 @@ function threeFlip() {
 
 	echo HHH percent: $( percentage $tripleHeadCount $noFlip ), TTT percent: $( percentage $tripleTailCount $noFlip ), HHT percent: $( percentage $headHeadTail $noFlip ), TTH percent: $( percentage $tailTailHead $noFlip ), HTH percent: $( percentage $headTailHead $noFlip ), THT percent: $( percentage $tailHeadTail $noFlip ) , THH percent: $( percentage $tailHeadHead $noFlip ), HTT percent: $( percentage $headTailTail $noFlip )
 }
+function main() {
+        echo "----------Flip Coin Simulation----------"
+	echo -e "Enter 1 to flip one coin \nEnter 2 to flip two coin \nEnter 3 to flip three coin"
+	read -p "Enter choice: " choice
+	read -p "How many times you want to flip the coin: " timesFlip
 
-echo "----------Flip Coin Simulation----------"
-echo -e "Enter 1 to flip one coin \nEnter 2 to flip two coin \nEnter 3 to flip three coin"
-read -p "Enter choice: " choice
-read -p "How many times you want to flip the coin: " timesFlip
-
-case $choice in
-	1)
-		oneFlip $timesFlip;;
-	2)
-		twoFlip $timesFlip;;
-	3)
-		threeFlip $timesFlip;;
-	*)
-		echo "Invalid choice!!";;
-esac 
+	case $choice in
+		1)
+			oneFlip $timesFlip;;
+		2)
+			twoFlip $timesFlip;;
+		3)
+			threeFlip $timesFlip;;
+		*)
+			echo "Invalid choice!!";;
+	esac 
+}
+main
